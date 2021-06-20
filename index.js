ce063c41f84b9eb227235035a11cb54dade6c948
gustavohdev <g_a93@outlook.com>
2021-06-20 20:43:56 -0300

    prettify index.js

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const routes = require('./routes/api');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes/api");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://admin:admin@oficina.busoo.mongodb.net/oficina?retryWrites=true&w=majority`);
 
-
 app.use(bodyParser.json());
-app.use('/api', routes);
+app.use("/api", routes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 
-  const path = require('path');
-  app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`)
+    console.log(`app running on port ${PORT}`);
 });
